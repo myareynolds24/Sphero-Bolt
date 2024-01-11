32f26ece9597e39c7f17228b764f6b80e7ce33c8
Mya <02mya.reynolds@gulllakecs.org>
2024-01-11 08:13:07 -0500

    Hello world connected and stable

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,12 @@
 //Programmer: Mya
 //Date: 1.10.2024
 //Program: Sphero Bolt Testing
+//Bolt ID: SB-52F9
+async function startProgram() {
+	await speak("Hello World", true);
+	setMainLed({ r: 0, g: 0, b: 255});
+	setSpeed(60);
+	await delay (2);
+	setSpeed(0);
+
+}
