3096345df895fafde6a9b84732b8e234e01bdc65
Mya <02mya.reynolds@gulllakecs.org>
2024-01-19 08:40:31 -0500

    square and colors finished
    yaaa

--- sandbox.js
+++ sandbox.js
@@ -3,36 +3,23 @@
 //Program: Sphero Bolt Testing
 //Bolt ID: SB-52F9
 
-/*
-async function //Programmer: Avonlea Thalmann
-startProgram() {
-	await speak("Hello World", true);
-	setMainLed({ r: 0, g: 0, b: 255});
-	setSpeed(60);
-	await delay (2);
-	setSpeed(0);
-}
-*/
- 
 //Square w/ different colors for each line & commit
 async function startProgram() {
-	await roll (0, 50, 2)
-	await roll (90, 50, 2)
-	setHeading(90)
-	setMainLed ({ r:0, g: 0, b: 255});
-	stopRoll
-	await roll (180, 50, 2)
-	setHeading(90)
-	setMainLed ({ r:0, g: 255, b: 0});
-	stopRoll
-	await roll (270, 50, 2)
-	setHeading(90)
-	setMainLed ({ r:255, g: 0, b: 0});
-	stopRoll
-	await roll (360, 100, 2)
-	setHeading(90)
-	setMainLed ({ r:0, g: 0, b: 255});
-	stopRoll
+setMainLed ({ r:100, g: 0, b: 100});
+await roll(0, 50, 2)
+setMainLed ({ r:44, g: 49, b: 0});
+await delay (1)
+await roll (90, 50, 2)
+setMainLed ({ r:0, g: 0, b: 255});
+await delay (1)
+await roll (180, 50, 2)
+setHeading(180)
+setMainLed ({ r:0, g: 255, b: 0});
+await delay (1)
+await roll (270, 50.9, 2)
+setMainLed ({ r:255, g: 0, b: 0});
+await delay (1)
+stopRoll
 }
 //Square w/ different colors & sounds for each line & commit
  
