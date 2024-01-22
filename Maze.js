bb8c90078c9e721a350b55fd6baf3ef7bddccd24
Mya <02mya.reynolds@gulllakecs.org>
2024-01-22 08:40:18 -0500

    start sound and message

--- Maze.js
+++ Maze.js
@@ -3,3 +3,8 @@
 // Date: 1.22.2024
 // Program: Maze
 // Bolt ID: SB-52F9
+//Start message
+async function startProgram() {
+await scrollMatrixText('Start', { r: 66, g: 56, b: 255 }, 30, true)
+await speak("Start", true);
+}
