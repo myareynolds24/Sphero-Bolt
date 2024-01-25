2057e0948d100f0134714b507db63e3b7e26840d
Mya <02mya.reynolds@gulllakecs.org>
2024-01-25 07:37:49 -0500

    blue checkpoint 1

--- Maze.js
+++ Maze.js
@@ -4,7 +4,16 @@
 // Program: Maze
 // Bolt ID: SB-52F9
 //Start message
+/*
+// Start Maze
 async function startProgram() {
 await scrollMatrixText('Start', { r: 66, g: 56, b: 255 }, 30, true)
 await speak("Start", true);
+*/
+
+//Blue: Checkpoint 1
+async function startProgram() {
+	await roll(0, 50, 5)
+	await delay (3)
+	setMainLed({ r: 0, g: 0, b: 255 });
 }
