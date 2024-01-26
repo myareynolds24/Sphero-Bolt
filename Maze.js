7a86b53e7d51998f310072ec5ffadddea22746be
Mya <02mya.reynolds@gulllakecs.org>
2024-01-26 07:39:54 -0500

    play animal sound 2

--- Maze.js
+++ Maze.js
@@ -5,15 +5,20 @@
 // Bolt ID: SB-52F9
 //Start message
 /*
+/*
 // Start Maze
 async function startProgram() {
 await scrollMatrixText('Start', { r: 66, g: 56, b: 255 }, 30, true)
 await speak("Start", true);
-*/
+
 
 //Blue: Checkpoint 1
-async function startProgram() {
-	await roll(0, 50, 5)
-	await delay (3)
+	await roll(0, 50, 3)
+	await delay (1)
 	setMainLed({ r: 0, g: 0, b: 255 });
-}
+	*/
+//Checkpoint 2: Sound 1
+async function startProgram() {
+	await roll (90, 50, 2)
+	await delay (1)
+	await Sound.Animal.play(true)
